47c04acff2fd74cc405888c30e3c4c80e4e9d64a
Dorcas29 <dorcasmutyavaviri@gmail.com>
2024-07-14 09:13:22 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Mugodhi-AFFM-Hymn-Book
 Mugodhi-AFFM-Hymn-Book
+
+
+###Aim
+* Build a Hymn book app with streamlit for My Churc Songs Lyrics
+
+#### Structure
+* Hymn Section
+* Favorite Hymn Section
+
+#### Installed Pkgs
+* pip3 install streamlit pandas spacy textblob neattext nltk altair matplotlib
+
---
+++ app.py
@@ -0,0 +1,32 @@
+# Core Pkgs
+import streamlit as st
+import streamlit.components.v1 as stc
+
+# EDA Pkgs
+import pandas as pd
+import neattext.functions as nfx
+
+
+# Data Viz Pkgs
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
+import altair as alt
+
+def main():
+    st.title("Mugodhi AFM Hymn Book")
+    menu = ["Home","Hymn","About"]
+
+    choice = st.sidebar.selectbox("Menu",menu)
+    if choice == "Home":
+        st.subheader("Favorite")
+    elif choice == "Hymn":
+        st.subheader("Hymn Retrieval")
+    else:
+        st.subheader("About")
+
+
+if _name_ == '_main_':
+    main()
+
+
