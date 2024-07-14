8b58614606ffdb894fc298efce1d45777af682b0
Dorcas29 <dorcasmutyavaviri@gmail.com>
2024-07-14 09:57:16 -0700

    update

--- app.py
+++ app.py
@@ -7,6 +7,15 @@ import pandas as pd
 import neattext as nt
 import neattext.functions as nfx
 
+# Sample text
+text = "This is a sample text with #hashtag, @mention, and a URL: https://example.com"
+
+# Clean the text
+cleaned_text = nfx.remove_hashtags(nfx.remove_mentions(nfx.remove_urls(text)))
+
+print(cleaned_text)
+import neattext.functions as nfx
+
 
 # Data Viz Pkgs
 import matplotlib.pyplot as plt
